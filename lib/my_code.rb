87bc2597dd17d6f821b07bb28bc62ecf179f8d03
Marco McNulty <mmc90@hotmail.co.uk>
2019-06-30 16:46:27 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,19 @@
 # Your Code Here
+def map(array)
+  new_array = []
+  array.each do |element|
+    new_array.push(yield(element))
+  end
+  new_array
+end
+
+def reduce(array, starting_point=0)
+  result = starting_point
+  array.each do |element|
+    if element == true
+      result = true
+    end
+    result = yield(result, element)
+  end
+  result
+end
