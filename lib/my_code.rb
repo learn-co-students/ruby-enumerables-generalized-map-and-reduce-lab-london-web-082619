02d8a3cc1dd814929160e3987919daecb89c2727
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-07-07 19:03:21 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,24 @@
-# Your Code Here
+
+def map(source_array)
+  new = []
+    i = 0
+    while i < source_array.length do
+      new.push(yield(source_array[i]))
+      i += 1
+    end
+    return new
+end
+
+def reduce(source_array, starting_point = 0)
+  running_total = starting_point
+  i = 0
+  while i < source_array.length do
+    running_total = yield(source_array[i], running_total)
+    i += 1
+  end
+  if running_total == nil
+      return false
+  else
+    return running_total
+  end
+end
