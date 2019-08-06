a228104371cfa3dfa9f768751d8aae8c5ba55910
Christopher Kennedy <chris94kennedy@gmail.com>
2019-08-06 14:49:51 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,12 @@
-# Your Code Here
+def map(source_array)
+  new = []
+  i = 0 
+  while i < source_array.length do 
+    new << yield(source_array[i])
+    i += 1 
+  end 
+return new
+end 
+
+def reduce(source_array, starting point=0)
+  new = 
