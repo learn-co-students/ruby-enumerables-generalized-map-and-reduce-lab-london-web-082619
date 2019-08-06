78f18030cc8e17f7e42d8b49278d227520d76c44
Christopher Kennedy <chris94kennedy@gmail.com>
2019-08-06 15:21:44 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,5 +8,17 @@ def map(source_array)
 return new
 end 
 
-def reduce(source_array, starting point=0)
-  new = 
+
+    
+def reduce(source_array, starting_point=0)
+  sum_total = starting_point
+  i = 0 
+  while i < source_array.length do 
+    if source_array[i] == true
+      return true
+    end
+    sum_total = yield(sum_total, source_array[i])
+    i += 1 
+  end
+  return sum_total
+end
