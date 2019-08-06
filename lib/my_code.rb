db2169fa922690259a8d1dbf64ae4fa8562d8572
Sarah Drake <jussaradrake@outlook.com>
2019-08-06 20:39:11 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
 # Your Code Here
+def map(array)
+  new = []
+  counter = 0
+  while counter < array.length
+    new.push(yield(array[counter]))     #In Ruby, the yield keyword yields the flow of control to the code in the block.
+    counter +=1                            #So, the code in the block is executed and the execution continues after the
+  end                                 #line containing the yield.
+  new
+end
+
+def reduce(array, sp=0) #sp= start point
+  if sp
+    accumulator = sp
+    counter = 0
+  else
+    accumulator = array[0]
+    counter = 1
+end
+while counter < array.length
+  accumulator = yield(accumulator, array[counter])
+  counter += 1
+end
+accumulator
+end
