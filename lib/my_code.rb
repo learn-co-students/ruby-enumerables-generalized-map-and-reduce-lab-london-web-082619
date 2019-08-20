9725515dfa375e52a065ba2615769fa4881c97eb
Takeshi Kimoto <lnuk2009jp@gmail.com>
2019-08-20 01:17:29 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,6 +3,31 @@
 #new_array = array.map{|n| n * -1}
 #new_array
 #end
-array = [1,2,3,-9]
-new_array = array.map{|n| n * -1}
-new_array
+#array = [1,2,3,-9]
+#new_array = array.map{|n| n * -1}
+#new_array
+
+def map(s)
+  new = []
+  i = 0
+  while i < s.length
+    new.push(yield(s[i]))
+    i += 1
+  end
+  new
+end
+
+def reduce(s, sp=nil)
+  if sp
+    accum = sp
+    i = 0
+  else
+    accum = s[0]
+    i = 1
+  end
+  while i < s.length
+    accum = yield(accum, s[i])
+    i += 1
+  end
+  accum
+end
