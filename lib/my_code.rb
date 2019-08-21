382f71e8d1df54decb1f074d49ab04d652f7e4a0
Andrius Jurgsaitis <andriusj6@hotmail.com>
2019-08-21 21:45:41 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,28 @@
 # Your Code Here
+
+def map(source_array)
+  new_array = []
+
+    source_array.length.times { |index|
+    new_array.push(yield(source_array[index]))
+    }
+    
+  new_array
+end
+
+def reduce(source_array, starting_point = nil)
+  if starting_point
+    total = starting_point
+    counter = 0
+  else
+    total = source_array[0]
+    counter = 1
+  end
+  
+  while counter < source_array.length do
+    total = yield(total, source_array[counter])
+    counter += 1
+  end
+
+  total
+end
