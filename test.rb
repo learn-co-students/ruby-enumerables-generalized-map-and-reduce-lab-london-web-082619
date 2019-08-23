e8bc7ff3c1c1b2568d6b50f43599dcaa68abea32
Takeshi Kimoto <lnuk2009jp@gmail.com>
2019-08-23 09:04:42 +0000

    Done.

--- test.rb
+++ test.rb
@@ -1,4 +1,4 @@
-def map(array)
+def map_test(array)
   new_map = []
   i = 0
   while i < array.length
@@ -10,5 +10,23 @@ end
 
 puts map_test([1,2,3,4,5]){|j| j*j }
 
+# {|j| j*j }  = called by yield. Method gives array[i] as argument. the argument is redefined in {} as j
+
+def reduce(array, starting = 0)
+  if starting
+    accum = starting
+    i = 0
+  else
+    accum = array[0]
+    i = 1
+  end
+     while i < array.length do
+     accum = yield(accum, array[i])
+     i += 1
+     end
+  accum
+end
+
+puts reduce([1,2,3,4,5]){|sum, j| sum + j }
 
 # {|j| j*j }  = called by yield. Method gives array[i] as argument. the argument is redefined in {} as j
